f9359770f1b8d51dcec6747a36c20b4f64ef9426
Tarcisio Silva <tarcisior19@hotmail.com>
2020-08-02 23:07:10 -0300

    feat : Impelentando o delete

---
+++ api/delete.php
@@ -0,0 +1,36 @@
+<?php
+require('../config.php');
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+if($method === 'delete'){
+
+    parse_str(file_get_contents('php://input'), $input);
+
+    $id = (!empty($input['id'])) ? $input['id'] : null;
+   
+
+    $id = filter_var($id);
+   
+
+    if($id){
+        $sql = $pdo->prepare("SELECT * FROM registers WHERE id = :id");
+        $sql->bindValue(":id", $id);
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+            $sql = $pdo->prepare("DELETE FROM registers WHERE id = :id" );
+            $sql->bindValue(":id",$id);
+            $sql->execute();
+        }else{
+            $array['error'] = "Contestação não localizada!";    
+        }
+    }else{
+        $array['error'] = "ID invalidos";
+    }
+
+
+  
+}else{
+    $array['error'] = 'Metodo não permitido (apenas DELETE)';
+}
+
+require('../return.php');
